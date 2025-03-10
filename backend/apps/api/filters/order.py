5a36fe58d70959955f0d03ebf630c61861368e21
Kryštof Kiss <krystof.kiss@backbone.sk>
2025-03-10 09:34:12 +0100

    filter order by order_number and customer_name

--- backend/apps/api/filters/order.py
+++ backend/apps/api/filters/order.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from apps.core.models import Order
 
@@ -8,5 +9,8 @@ class OrderFilter(django_filters.FilterSet):
         model = Order
         fields = []
 
-    customer_name = django_filters.CharFilter(lookup_expr="icontains")
+    text_query = django_filters.CharFilter(method="filter_by_order_and_name")
     date = django_filters.DateFilter(field_name="date", lookup_expr="date")
+
+    def filter_by_order_and_name(self, queryset, name, value):
+        return queryset.filter(Q(customer_name__icontains=value) | Q(order_number__icontains=value))
